073ebcd353716182f8eac8f18de0e8248c2169bb
Tommaso Cavana <tommaso.cavana@gmail.com>
2021-01-20 17:55:19 +0100

    Done: -wrote functions comments a bit better -changed the to orbitals combination to a dict. So I can access them with .get() -changed some (not all) stray values into variables with speaking names -left the solution with try-except in quotes -changed some names, but still to improve
    To do:
    -change the variables names in a proper way
    -decide which entities have to global, so it becomes much clearer

--- Program.py
+++ Program.py
@@ -1,91 +1,102 @@
+
 import numpy as np
 import pandas as pd
 from numpy import linalg
-#from numpy import linalg
-import timeit
 import matplotlib.pyplot as plt
 %matplotlib inline
 plt.style.use('seaborn-whitegrid')
 
-
-Es= -8.0 #-8.0
-Ep= 0.0 #0.0
-Vss = -2.0 #-2.0
+"""these need to be put in a different script"""
+Es= -8.0
+Ep= 0.0
+Vss = -2.0
 Vpp_sigma = +2.2
 Vpp_pi = -1.8
 Vsp = -2.1
 
-#if you need to add orbitals, just add the (spx,spy...) and the orbitals in "orbitals"
 
 def K_pathes(*path): #put a way to have just the G,X,M letters inserted!
-    """function that takes 'G', 'X' OR 'M' letters (with up-commas)
-    and return a tuple with k*a values along x and y directions"""
+    """takes 'G', 'X' OR 'M' as chars (with up-commas)
+    create through 'path_dict' the k pathes.
+    return a tuple with k*a values along x and y directions"""
 
     path_dict = {'G' : (0,0), 'X' : (np.pi,0), 'M' : (np.pi,np.pi)}
+
     k_path_x=[]
     k_path_y=[]
-    precision=150 #how many steps for each couple of letters
-    #path_dict['G'][0]
-    for index in range(len(path)-1): #we have 2 arrays, with the total k path in x and y dim. There is surely a better way!
+    precision=100 #how many steps for each couple of letters
+    for index in range(len(path)-1):
         k_path_x = np.concatenate((k_path_x, np.linspace(path_dict[path[index]][0],path_dict[path[index+1]][0], precision)))
         k_path_y = np.concatenate((k_path_y, np.linspace(path_dict[path[index]][1],path_dict[path[index+1]][1], precision)))
     return k_path_x, k_path_y
 
 
 def Ham_matrix(kx_a, ky_a): #qua potrei creare un oggetto matrice, cioè fare una classe.. invece che inizializzare ogni volta N matrici
-    """it takes the comined orbitals as arguments (ss,spx,spy..)
-    then creates a square matrix with the length of the orbitals
-    then it becomes a DataFrame, with indexes and columns equal to orbitals names
-    it inizializes with a double cycle each matrix element with the corresponding value (uses the name of the indexes)
-    return the DataFrame inizialized
-    return a matrix with values inizialized"""
+    """takes the k_pathes along x and y (multiplied by Ham vector of reciprocal lattice, 'a')
+    inizitalizes the parameters (Hamiltonian matrix elements) with kx_a and ky_a values
+    creates a DataFrame with indices and columns as 'orbitals' (s, px, py, pz)
+    inizializes the Dataframe by orbitals' names with the parameters
+    return the DataFrame"""
 
-    ss = Es + 2*Vss*(np.cos(kx_a)+np.cos(ky_a))
-    spx = 2*Vsp*np.sin(kx_a)
-    spy = 2*Vsp*np.sin(ky_a)
-    pxpx = Ep + 2*(Vpp_sigma*np.cos(kx_a)+Vpp_pi*np.cos(ky_a))
-    pypy = Ep + 2*(Vpp_sigma*np.cos(ky_a)+Vpp_pi*np.cos(kx_a))
-    pzpz = Ep + 2*Vpp_pi*(np.cos(kx_a)+np.cos(ky_a))
-    #all above could be put out
+    #parameters to be set in Hamiltonian matrix
+    orbitals_dict = {
+    'ss' : Es + 2*Vss*(np.cos(kx_a)+np.cos(ky_a)),
+    'spx' : 2*Vsp*np.sin(kx_a),
+    'spy' : 2*Vsp*np.sin(ky_a),
+    'pxpx' : Ep + 2*(Vpp_sigma*np.cos(kx_a)+Vpp_pi*np.cos(ky_a)),
+    'pypy' : Ep + 2*(Vpp_sigma*np.cos(ky_a)+Vpp_pi*np.cos(kx_a)),
+    'pzpz' : Ep + 2*Vpp_pi*(np.cos(kx_a)+np.cos(ky_a))
+    }
 
+    #create the Dataframe that have the parameters
     orbitals = np.array(['s','px','py','pz'])
-    basic_matrix = np.zeros(shape = (len(orbitals), len(orbitals)), dtype = float)
-    final_matrix = pd.DataFrame(basic_matrix, index=orbitals, columns=orbitals)
+    matrix_order = len(orbitals)
+    matrix = np.zeros(shape = (matrix_order, matrix_order))
+    matrix = pd.DataFrame(matrix, index=orbitals, columns=orbitals)
 
-    #now inizialize every matrix elements with the names "spx,spy.."
-    """top funziona"""
+    #inizialize Hamiltonian matrix thorugh parameters' name
     for i in orbitals:
         for j in orbitals:
+            mixed_orbitals = i+j
+            matrix_elements = orbitals_dict.get(mixed_orbitals)
+            if (matrix_elements == None):
+                print(f"{mixed_orbitals} has null values")
+            else:
+                matrix.loc[i,j] = orbitals_dict[mixed_orbitals]
+                matrix.loc[j,i] = orbitals_dict[mixed_orbitals]
+                """
             try:
-                final_matrix.loc[i,j] = eval(i+j)
-                final_matrix.loc[j,i] = eval(i+j)
-            except NameError: #i think that this shouldn't be done.. it's nasty
+                matrix.loc[i,j] = orbitals_dict[mixed_orbitals]
+                matrix.loc[j,i] = orbitals_dict[mixed_orbitals]
+            except KeyError: #i think that this shouldn't be done.. it's nasty
                 pass
-    return final_matrix
+                """
+    return matrix
 
-Ham_matrix(0,0)
 
-#k_a=np.linspace(0,np.pi,100) this is old!
+def eigenvalues_path(*critical_points): #put a variable that can be inserted by keyboard or script
+    """take char values (only 'X','G' or 'M')
+    calculate the k pathes
+    calculate the eigenvalues for each k point and concatenate them
+    reshape such that each row has eingevalues relative to just a k point
+    return eigenvalues and length of k path"""
+    #I DO NOT LIKE THAT INPUT IN K_pathes IS THE SAME OF THE MAIN FUNCTION! BAD!
+    #create k pathes
+    (kx_a, ky_a)= K_pathes(*critical_points)
 
-def eigenvalues_path():
-    """calculate the k pathes given the X,G,M labels (just those!)"""
-    (kx_a, ky_a)= K_pathes('X','G','M','X','G') #forse è meglio tenerlo fuori dalla funzione
-    eigenvals = []
-
+    #calculate the eigenvalues for each k point
+    eigenvalues = []
     for i in range(len(kx_a)):
-        """calculate eigenvalues for each matrix with a certain kx_a, ky_a values
-        append the values to 'eigenvals'
-        return eigenvalues"""
-        eigenvals_temporary = linalg.eigvalsh(Ham_matrix(kx_a[i], ky_a[i]))
-        eigenvals = np.concatenate((eigenvals, eigenvals_temporary))
+        eigenvalues_temporary = linalg.eigvalsh(Ham_matrix(kx_a[i], ky_a[i]))
+        eigenvalues = np.concatenate((eigenvalues, eigenvalues_temporary))
 
-    return eigenvals, len(kx_a)
+    n_of_k_points = len(kx_a)
+    n_orbitals = 4 #maybe i can put 'orbitals' global
+    eigenvalues = np.reshape(eigenvalues, newshape=(n_of_k_points, n_orbitals)) #each row has eigenvalues corresponding to a k point
 
-bands, k_length = eigenvalues_path()
-bands = np.reshape(bands, newshape=(k_length,4))
-bands[34:39, :]
-np.shape(bands)
-k_length
+    return eigenvalues, n_of_k_points
+
+eigenvalues, n_of_k_points = eigenvalues_path('X','G','M','X','G')
 
 #%% #to run use ctrl+alt+enter
 """graphical part"""
@@ -100,10 +111,10 @@ x=[0,100,200,300,400]
 my_xticks = ['X','G','M','X','M']
 plt.xticks(x, my_xticks)
 
-ax.plot(range(k_length), bands[:,0], color='blue');
-ax.plot(range(k_length), bands[:,1], color='blue');
-ax.plot(range(k_length), bands[:,2], color='blue');
-ax.plot(range(k_length), bands[:,3], color='blue');
+ax.plot(range(n_of_k_points), eigenvalues[:,0], color='blue');
+ax.plot(range(n_of_k_points), eigenvalues[:,1], color='blue');
+ax.plot(range(n_of_k_points), eigenvalues[:,2], color='blue');
+ax.plot(range(n_of_k_points), eigenvalues[:,3], color='blue');
 plt.legend()
-ax.set(ylim=(-24, 12))
+#ax.set(ylim=(-24, 12))
 #%%
